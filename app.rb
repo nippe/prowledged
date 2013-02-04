b04fbd74f3f29cb97a8284b3b229027f7c371b9c
Niklas Nihlen <niklas.nihlen@gmail.com>
2013-02-04 08:02:50 +0100

    Started to refactor out som methods and adding annother route to support priority of the Prowl message throug the URL

--- app.rb
+++ app.rb
@@ -7,21 +7,44 @@ get '/' do
   'Hey hey'
 end
 
-post '/:api_key/notification/send' do
-  content_type :json
+def get_severity(severity)
+  if(severity.nil?)
+    Prowly::Notification::Priority::MODERATE
+  else
+    Prowly::Notification::Priority::EMERGENCY
+  end
+end
 
+def send_notification(indata)
   Prowly.notify do |n|
-    n.apikey = params[:api_key]
-    n.priority = Prowly::Notification::Priority::EMERGENCY
+    n.apikey = indata[:api_key]
+    n.priority = get_severity(indata[:priority])
     n.application = "Cosm"
-    n.event = params[:event]
-    n.event = "#{params[:datastream]} is #{params[:value]}"
-    n.description = params[:description]
+    n.event = indata[:event]
+    n.event = "#{indata[:datastream]} is #{indata[:value]}"
+    n.description = indata[:description]
     n.url = 'http://cosm.com/feeds/100109'
   end
+end
+
+
+def set_return_message_and_status
+  content_type :json
 
   status 200
   {:status => 'OK', :status_description => "Message Sent"}.to_json
 end
+
+post '/:api_key/notification/send' do
+  send_notification(params)
+  set_return_message_and_status()
+end
+
+
+post '/:api_key/notification/send/:priority' do
+  send_notification(params)
+  set_return_message_and_status()
+end
+
 
 
